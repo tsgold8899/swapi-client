0e4c55ba8ff2dc21af2df5284aaff6d9eaa4e0ee
Ming Yao <tsgold8899@gmail.com>
2022-10-30 11:52:35 -0400

    test app

--- api/app.py
+++ api/app.py
@@ -6,7 +6,6 @@ from flask import request
 from swapi import SWApi
 
 dev_mode = True
-app = Flask(__name__)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -14,70 +13,76 @@ logging.basicConfig(
     format="%(levelname)s %(asctime)s %(message)s",
 )
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def index() -> dict:
-    return {
-        "data": "Hello, welcome to Sleuth backend interview task. Please see instructions in README.md"
-    }
-
-
-@app.route("/health")
-def health_check() -> dict:
-    response = SWApi().get("/")
-    return {"swapi_is_healthy": response.status_code == 200, "urls": response.json()}
+    @app.route("/")
+    def index() -> dict:
+        return {
+            "data": "Hello, welcome to Sleuth backend interview task. Please see instructions in README.md"
+        }
 
 
-@app.route("/swapi/vehicles", methods=["GET"])
-def swapi_min_cargo_capacity_vehicles() -> dict:
-    """Lists vehicles with cargo capacity that is greater or equal to the provided value.
+    @app.route("/health")
+    def health_check() -> dict:
+        response = SWApi().get("/")
+        return {"swapi_is_healthy": response.status_code == 200, "urls": response.json()}
 
-    For each vehicle list the attrs: name, model and cargo capacity
-    """
-    # Validate min_cargo_capacity param
-    min_cargo_capacity = request.args.get("min_cargo_capacity")
-    if min_cargo_capacity:
-        try:
-            min_cargo_capacity = int(min_cargo_capacity)
-        except:
-            return "Invalid Request", 400
-    else:
-        min_cargo_capacity = None
 
-    # Collecting vehicles meeting condition
-    vehicles = []
-    params = {
-        "page": 1
-    }
-    while True:
-        try:
-            response = SWApi().get("/vehicles", params)
-            if response.status_code == 200:
-                data = response.json()
-                for vehicle in data["results"]:
-                    cargo_capacity = None
-                    try:
-                        cargo_capacity = int(vehicle["cargo_capacity"])
-                    except:
-                        pass
-                    if min_cargo_capacity is None or (cargo_capacity is not None and cargo_capacity >= min_cargo_capacity):
-                        vehicles.append({
-                            "name": vehicle["name"],
-                            "model": vehicle["model"],
-                            "cargo_capacity": vehicle["cargo_capacity"],
-                        });
-                if data["next"] is None:
-                    break
-                params["page"] += 1
-            else:
-                return response.text, response.status_code
-        except:
-            return "Not Reachable", 404
+    @app.route("/swapi/vehicles", methods=["GET"])
+    def swapi_min_cargo_capacity_vehicles() -> dict:
+        """Lists vehicles with cargo capacity that is greater or equal to the provided value.
 
-    return {
-        "available_vehicles": vehicles
-    }
+        For each vehicle list the attrs: name, model and cargo capacity
+        """
 
+        # Validate min_cargo_capacity param
+        min_cargo_capacity = request.args.get("min_cargo_capacity")
+        if min_cargo_capacity:
+            try:
+                min_cargo_capacity = int(min_cargo_capacity)
+            except:
+                return "Invalid Request", 400
+        else:
+            min_cargo_capacity = None
+
+        # Collecting vehicles meeting condition
+        vehicles = []
+        params = {
+            "page": 1
+        }
+        while True:
+            try:
+                response = SWApi().get("/vehicles", params)
+                if response.status_code == 200:
+                    data = response.json()
+                    for vehicle in data["results"]:
+                        cargo_capacity = None
+                        try:
+                            cargo_capacity = int(vehicle["cargo_capacity"])
+                        except:
+                            pass
+                        if min_cargo_capacity is None or (cargo_capacity is not None and cargo_capacity >= min_cargo_capacity):
+                            vehicles.append({
+                                "name": vehicle["name"],
+                                "model": vehicle["model"],
+                                "cargo_capacity": vehicle["cargo_capacity"],
+                            });
+                    if data["next"] is None:
+                        break
+                    params["page"] += 1
+                else:
+                    return response.text, response.status_code
+            except:
+                return "Not Reachable", 404
+
+        return {
+            "available_vehicles": vehicles
+        }
+
+    return app
+
+main = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    main.run(host="0.0.0.0", port=5001)
---
+++ api/test_app.py
@@ -0,0 +1,52 @@
+from mock import patch, Mock
+from requests import Response
+import pytest
+
+from app import create_app
+from swapi import SWApi
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_swapi_min_cargo_capacity_vehicles(client):
+    url_to_test = "/swapi/vehicles"
+    response = SWApi().get("/vehicles")
+    total = response.json()["count"]
+    
+    with client.get(url_to_test) as res:
+        assert res.status_code == 200
+        assert len(res.json["available_vehicles"]) == total
+
+    with client.get(url_to_test, query_string={"min_cargo_capacity": ""}) as res:
+        assert res.status_code == 200
+        assert len(res.json["available_vehicles"]) == total
+
+    with client.get(url_to_test, query_string={"min_cargo_capacity": "a"}) as res:
+        assert res.status_code == 400
+
+    min_cargo_capacity = 50000
+    with client.get(url_to_test, query_string={"min_cargo_capacity": min_cargo_capacity}) as res:
+        assert res.status_code == 200
+        for vehicle in res.json["available_vehicles"]:
+            assert int(vehicle["cargo_capacity"]) >= min_cargo_capacity
+
+    with patch('swapi.SWApi.get', side_effect=Exception("error")):
+        with client.get(url_to_test) as res:
+            assert res.status_code == 404
+
+    with patch('swapi.SWApi.get') as mock_get:
+        mock_response = Response()
+        mock_response.status_code = 403
+        mock_get.return_value = mock_response
+        with client.get(url_to_test) as res:
+            assert res.status_code == 403
